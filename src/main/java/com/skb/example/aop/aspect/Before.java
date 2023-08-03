2cf5d20d03786b97c5682aa4f0ff01fb99583cac
unknown <shakibahmed09112017@gmail.com>
2023-08-03 18:29:50 +0600

    aspect add

---
+++ src/main/java/com/skb/example/aop/aspect/Before.java
@@ -0,0 +1,12 @@
+package com.skb.example.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Before {
+
+
+}
